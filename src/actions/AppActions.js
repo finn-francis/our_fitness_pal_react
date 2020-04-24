d707b741b06a556a1952340bf10da465c4e11750
Finn Francis <finnfrancis123@gmail.com>
2020-04-24 11:57:59 +0100

    Created action for setCurrentUser

---
+++ src/actions/AppActions.js
@@ -0,0 +1,13 @@
+import dispatcher from "../dispatcher/AppDispatcher"
+
+// Gives us an action to call for setCurrentUser
+// Usage:
+// import {setCurrentUser} from '../actions/AppActions'
+// const user = {id: 3, email: "user@email.com"}
+// setCurrentUser(user)
+export const setCurrentUser = (user) => {
+  dispatcher.dispatch({
+    type: 'setCurrentUser',
+    user: user
+  })
+}
