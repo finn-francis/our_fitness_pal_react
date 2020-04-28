3f13781dbd7a1b9bc3da33e000b55ff22c555629
Finn Francis <finnfrancis123@gmail.com>
2020-04-28 16:25:35 +0100

    Removed unused dependencies from tests

--- src/components/exercise/__tests__/FormModal.test.js
+++ src/components/exercise/__tests__/FormModal.test.js
@@ -1,13 +1,10 @@
-import { enableFetchMocks } from 'jest-fetch-mock'
-enableFetchMocks()
-
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import FormModal from '../FormModal'
 
