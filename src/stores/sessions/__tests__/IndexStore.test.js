cdae034f4ac334fae81a114f581a657ab16b6fca
Finn Francis <finnfrancis123@gmail.com>
2020-05-04 15:18:25 +0100

    Updated the session index store to allow it to be updated
    How?
    - Registered the store with the dispatcher, creating a handleActions function for dealing with the actions.
    - Created a function setSessions which takes a list of sessions, sets the new value and emits a change event.

--- src/stores/sessions/__tests__/IndexStore.test.js
+++ src/stores/sessions/__tests__/IndexStore.test.js
@@ -1,13 +1,31 @@
 import IndexStore from '../IndexStore'
 
 describe('IndexStore', () => {
-  describe('#getAll', () => {
-    let sessions = [{id: 1, name: 'Leg Day', description: 'this is the day we do legs'}]
+  let sessions = [{id: 1, name: 'Leg Day', description: 'this is the day we do legs'}]
+  afterEach(() => IndexStore.sessions = [])
 
+  describe('#getAll', () => {
     beforeEach(() => IndexStore.sessions = sessions)
-    afterEach(() => IndexStore.sessions = [])
 
     it('should return a list of sessions', () => {
+      expect(IndexStore.getAll()).toEqual(sessions)
+    })
+  })
+
+  describe('#setSessions', () => {
+    it('should update the list of sessions', () => {
+      expect(IndexStore.getAll()).toEqual([])
+      IndexStore.setSessions(sessions)
+
+      expect(IndexStore.getAll()).toEqual(sessions)
+    })
+  })
+
+  describe('#handleActions', () => {
+    describe('with setSessions as an argument', () => {
+      expect(IndexStore.getAll()).toEqual([])
+      IndexStore.handleActions({type: 'setSessions', sessions: sessions})
+
       expect(IndexStore.getAll()).toEqual(sessions)
     })
   })
