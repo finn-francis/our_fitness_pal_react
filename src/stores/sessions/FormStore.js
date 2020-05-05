7d0d780a56a5dca67d55497cce10b4ae9671ea1e
Finn Francis <finnfrancis123@gmail.com>
2020-05-05 10:15:26 +0100

    Created a form store for sessions
    How?
    - Added a session form object.
    - Created methods for getting, clearing and updating the session object.

---
+++ src/stores/sessions/FormStore.js
@@ -0,0 +1,50 @@
+import {EventEmitter} from 'events'
+import dispatcher from '../../dispatcher/AppDispatcher'
+
+class FormStore extends EventEmitter {
+  constructor(props) {
+    super(props)
+    this.clearSession()
+  }
+
+  clearSession() {
+    this.sessionObject = {
+      id: null,
+      name: "",
+      description: "",
+      errors: {},
+      formAction: 'new',
+      responseSuccess: null
+    }
+  }
+
+  get() {
+    return this.sessionObject
+  }
+
+  updateSessionForm(session) {
+    for (const [key, value] of Object.entries(session)) {
+      this.sessionObject[key] = value
+    }
+    this.emit("change")
+  }
+
+  handleActions(action) {
+    switch(action.type) {
+      case "setSession":
+        this.updateSessionForm(action.session)
+        break
+      case "clearSessionForm":
+        this.clearSession()
+        this.emit("change")
+        break
+      default:
+        break
+    }
+  }
+}
+
+const formStore = new FormStore()
+dispatcher.register(formStore.handleActions.bind(formStore))
+
+export default formStore
