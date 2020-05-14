2c674c5e58fa810efd2221e14a5d125ba695f857
Finn Francis <finnfrancis123@gmail.com>
2020-05-14 11:20:18 +0100

    Added 'view' links to all session items
    How?
    - Used ReactRouter Links to link to the correct show page.
    - Removed the index component test as everything is covered in the cypress spec and adding Links causes difficulties with ReactRouter.

--- cypress/integration/sessions/show.spec.js
+++ cypress/integration/sessions/show.spec.js
@@ -6,17 +6,44 @@ describe('Viewing the show page for a session', () => {
   const sessionPath = `/sessions/${session.id}`
   const sessionUrl = Cypress.env('apiUrl') + sessionPath
 
-  beforeEach(() => {
-    cy.server()
-    cy.route('GET', sessionUrl, {session: session})
-    cy.login(admin)
-    cy.visit(sessionPath)
+  context('navigating directoy to the page', () => {
+    beforeEach(() => {
+      cy.server()
+      cy.route('GET', sessionUrl, {session: session})
+      cy.login(admin)
+      cy.visit(sessionPath)
+    })
+
+    it.only('should allow the user to navigate to the show page by clicking a link on the index', () => {
+      cy.get('.session-title')
+        .should('contain', session.name)
+        .get('.session-description')
+        .should('contain', session.description)
+    })
   })
 
-  it.only('should allow the user to navigate to the show page by clicking a link on the index', () => {
-    cy.get('.session-title')
-      .should('contain', session.name)
-      .get('.session-description')
-      .should('contain', session.description)
+  context('navigating to the page from the index', () => {
+    const indexUrl = Cypress.env('apiUrl') + '/sessions'
+
+    beforeEach(() => {
+      cy.server()
+      cy.route('GET', indexUrl, {sessions: sessionsFixture})
+      cy.route('GET', sessionUrl, {session: session})
+      cy.login(admin)
+      cy.visit('/sessions')
+    })
+
+    it.only('should allow the user to navigate to the show page by clicking a link on the index', () => {
+      cy.get(`a.view-session[href="${sessionPath}"]`)
+        .click()
+
+      cy.location('pathname')
+        .should('eq', sessionPath)
+
+      cy.get('.session-title')
+        .should('contain', session.name)
+        .get('.session-description')
+        .should('contain', session.description)
+    })
   })
 })
--- src/components/sessions/IndexListItem.js
+++ src/components/sessions/IndexListItem.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from "react-router-dom";
 
 const IndexListItem = (props) => {
   return (
-    <li className="list-group-item session-list-item">{props.session.name}</li>
+    <li className="list-group-item session-list-item">
+      {props.session.name}
+
+      <Link to={`/sessions/${props.session.id}`} className="btn btn-info float-right view-session" >View</Link>
+    </li>
   )
 }
 IndexListItem.propTypes = {session: PropTypes.object.isRequired}
