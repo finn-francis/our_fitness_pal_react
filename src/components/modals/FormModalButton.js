2f69cc64de0d09278d97da788323795926e8c2fa
Finn Francis <finnfrancis123@gmail.com>
2020-05-14 13:44:39 +0100

    Allows the form modal button to accept a click handler

--- src/components/modals/FormModalButton.js
+++ src/components/modals/FormModalButton.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const FormModalButton = (props) => {
   return (
     <>
-      <button id={props.id} type="button" className="btn btn-info" data-toggle="modal" data-target={`#${props.modalId}`}>
+      <button id={props.id} type="button" className="btn btn-info" data-toggle="modal" data-target={`#${props.modalId}`} onClick={props.handleClick}>
         {props.children}
       </button>
     </>
