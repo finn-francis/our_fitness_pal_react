17f21b268973a46da5cae3137dfa723a60ed94b8
Finn Francis <finnfrancis123@gmail.com>
2020-05-15 15:44:14 +0100

    Added a delete button to the session show page
    How?
    - Added a button and confirmation modal.
    - Utilized the deleteSession action and ShowStore function.
    - Added a redirect once the session is deleted.

--- cypress/integration/sessions/delete.spec.js
+++ cypress/integration/sessions/delete.spec.js
@@ -3,16 +3,20 @@ import admin from '../../fixtures/users/admin.json'
 
 const session = sessionsFixture[0]
 const deleteUrl = Cypress.env('apiUrl') + `/sessions/${session.id}`
+const showUrl = deleteUrl
 
 describe('Deleting a session', () => {
-  context('from the index', () => {
-    const indexUrl = Cypress.env('apiUrl') + '/sessions'
+  const indexUrl = Cypress.env('apiUrl') + '/sessions'
 
+  beforeEach(() => {
+    cy.server()
+    cy.route('GET', indexUrl, {sessions: sessionsFixture})
+    cy.route('DELETE', deleteUrl, {session: session})
+    cy.login(admin)
+  })
+
+  context('from the index', () => {
     beforeEach(() => {
-      cy.server()
-      cy.route('GET', indexUrl, {sessions: sessionsFixture})
-      cy.route('DELETE', deleteUrl, {session: session})
-      cy.login(admin)
       cy.visit('/sessions')
       cy.get('.session-list-item')
         .should('have.length', 3)
@@ -28,6 +32,31 @@ describe('Deleting a session', () => {
 
       cy.get('.session-list-item')
         .should('have.length', 2)
+      cy.get('#session-list :nth-child(1)')
+        .should('not.contain', session.name)
+    })
+  })
+
+  context('from the show page', () => {
+    beforeEach(() => {
+      cy.route('GET', showUrl, {session: session})
+      cy.route('GET', indexUrl, {sessions: sessionsFixture.slice(1)})
+      cy.visit(`/sessions/${session.id}`)
+    })
+
+    it.only('should delete the session', () => {
+      cy.get('.delete-session')
+        .click('')
+
+      cy.get('.modal-content > .modal-footer > .btn-info')
+        .click()
+
+      cy.location('pathname')
+        .should('eq', '/sessions')
+
+      cy.get('.session-list-item')
+        .should('have.length', 2)
+
       cy.get('#session-list :nth-child(1)')
         .should('not.contain', session.name)
     })
